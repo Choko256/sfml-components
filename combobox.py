9d8b23018cd025be28fb4d67395d520a1b8e8559
Michael Chacaton <choko@chaksoft.fr>
2013-08-29 16:59:59 +0200

    Combo Box Skeleton

---
+++ combobox.py
@@ -0,0 +1,10 @@
+#-*- coding:utf8 -*-
+
+# TextBox for SFML Python binding
+
+import sfml as sf
+import base
+
+class TextBox(base.BaseComponent):
+	def __init__(self, name):
+		base.BaseComponent.__init__(self, name)
